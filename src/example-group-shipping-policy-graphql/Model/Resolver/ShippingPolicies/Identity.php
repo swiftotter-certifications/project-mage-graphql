a1868f4cb758bf300bbfa3120a76746cc64ecd04
Chris Nanninga <chrisn@swiftotter.com>
2022-11-02 11:13:19 -0500

    Add caching support for shipping policies query

---
+++ src/example-group-shipping-policy-graphql/Model/Resolver/ShippingPolicies/Identity.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace SwiftOtter\GroupShippingPolicyGraphQl\Model\Resolver\ShippingPolicies;
+
+use Magento\Framework\GraphQl\Query\Resolver\IdentityInterface;
+use SwiftOtter\GroupShippingPolicy\Model\GroupShippingPolicy;
+
+class Identity implements IdentityInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getIdentities(array $resolvedData): array
+    {
+        $ids = [];
+        foreach ($resolvedData as $policyData) {
+            if (isset($policyData['id'])) {
+                $ids[] = GroupShippingPolicy::CACHE_TAG . '_' . $policyData['id'];
+            }
+        }
+        return $ids;
+    }
+}
